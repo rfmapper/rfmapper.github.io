2f3142210b6a3f7710335e7fcd69c5e0e3da70be
Kyle Uithoven <kyle.uithoven@gmail.com>
2014-01-20 16:55:47 -0700

    map.js

---
+++ source/map.js
@@ -0,0 +1,13 @@
+google.maps.visualRefresh = true;	
+	
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(33.450, 112.0667)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);

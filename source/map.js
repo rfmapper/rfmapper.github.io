b086c7f269d364741b20cb1a56accbe4b375a4eb
Kyle Uithoven <kyle.uithoven@gmail.com>
2014-01-20 16:59:04 -0700

    Center on phoenix

--- source/map.js
+++ source/map.js
@@ -4,7 +4,7 @@ var map;
 function initialize() {
   var mapOptions = {
     zoom: 8,
-    center: new google.maps.LatLng(33.450, 112.0667)
+    center: new google.maps.LatLng(33.450, -112.0667)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);

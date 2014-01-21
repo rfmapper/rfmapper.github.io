b9601cfd9ac25a4534ce636894b92e0a2a1b7164
Kyle Uithoven <kyle.uithoven@gmail.com>
2014-01-20 17:03:28 -0700

    Update map.js

--- source/map.js
+++ source/map.js
@@ -3,7 +3,7 @@ google.maps.visualRefresh = true;
 var map;
 function initialize() {
   var mapOptions = {
-    zoom: 8,
+    zoom: 10,
     center: new google.maps.LatLng(33.450, -112.0667)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
